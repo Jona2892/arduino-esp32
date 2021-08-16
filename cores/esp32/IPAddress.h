a8924caccd54eef84e0c604824812fb5ae14c811
Jona2892 <74907451+Jona2892@users.noreply.github.com>
2021-08-16 15:50:55 +0200

    Update IPAddress.h

--- cores/esp32/IPAddress.h
+++ cores/esp32/IPAddress.h
@@ -26,11 +26,12 @@
 
 // A class to make it easier to handle and pass around IP addresses
 
-class IPAddress: public Printable
+class IPAddress : public Printable
 {
 private:
-    union {
-        uint8_t bytes[4];  // IPv4 address
+    union
+    {
+        uint8_t bytes[4]; // IPv4 address
         uint32_t dword;
     } _address;
 
@@ -38,7 +39,7 @@ private:
     // to the internal structure rather than a copy of the address this function should only
     // be used when you know that the usage of the returned uint8_t* will be transient and not
     // stored.
-    uint8_t* raw_address()
+    uint8_t *raw_address()
     {
         return _address.bytes;
     }
@@ -60,27 +61,27 @@ public:
     {
         return _address.dword;
     }
-    bool operator==(const IPAddress& addr) const
+    bool operator==(const IPAddress &addr) const
     {
         return _address.dword == addr._address.dword;
     }
-    bool operator==(const uint8_t* addr) const;
+    bool operator==(const uint8_t *addr) const;
 
     // Overloaded index operator to allow getting and setting individual octets of the address
     uint8_t operator[](int index) const
     {
         return _address.bytes[index];
     }
-    uint8_t& operator[](int index)
+    uint8_t &operator[](int index)
     {
         return _address.bytes[index];
     }
 
     // Overloaded copy operators to allow initialisation of IPAddress objects from other types
-    IPAddress& operator=(const uint8_t *address);
-    IPAddress& operator=(uint32_t address);
+    IPAddress &operator=(const uint8_t *address);
+    IPAddress &operator=(uint32_t address);
 
-    virtual size_t printTo(Print& p) const;
+    virtual size_t printTo(Print &p) const;
     String toString() const;
 
     friend class EthernetClass;
@@ -91,6 +92,7 @@ public:
     friend class DNSClient;
 };
 
-const IPAddress INADDR_NONE(0, 0, 0, 0);
+// const IPAddress INADDR_NONE(0, 0, 0, 0);
+const IPAddress IP_ADDR_NONE(0, 0, 0, 0);
 
 #endif
